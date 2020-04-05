08a4be6861bd49ef3cde90a33c2494fa9e2fd648
Derek Wang <guanghuiw@vmware.com>
2020-04-05 22:40:47 +0800

    extract ui-tile library

--- apps/review/src/app/app.tsx
+++ apps/review/src/app/app.tsx
@@ -1,59 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
-import styled from '@emotion/styled';
+import {
+  TileGrid,
+  Tile,
+  ImageWrapper,
+  Image,
+  TileTitle,
+  TileLeftCorner,
+  TileRightCorner
+} from '@nx-bghoard/review/ui-tile';
 
 import { Game } from '@nx-bghoard/api-interfaces';
 
-export const TileGrid = styled.div`
-  display: flex;
-  flex-flow: wrap;
-  justify-content: space-evenly;
-`;
-
-export const Tile = styled.div`
-  border: solid 1px #ccc;
-  border-radius: 5px;
-  overflow: hidden;
-  display: block;
-  min-width: 200px;
-  color: inherit;
-  text-decoration: none;
-  margin: 0.5em;
-  &:hover {
-    border-color: #5159ea;
-  }
-`;
-
-export const ImageWrapper = styled.div`
-  width: 200px;
-  height: 200px;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-bottom: solid 1px #ccc;
-`;
-
-export const Image = styled.img`
-  max-width: 100%;
-  min-height: 100%;
-`;
-
-export const TileTitle = styled.h2`
-  text-align: center;
-`;
-
-export const TileLeftCorner = styled.div`
-  display: block;
-  float: left;
-  margin: -0.5em 0 1em 0.5em;
-`;
-
-export const TileRightCorner = styled.div`
-  display: block;
-  float: right;
-  margin: -0.5em 0.5em 1em 0;
-`;
 export const currencyFormat = (amount: number) => '$' + amount.toFixed(2);
 export const ratingFormat = (rating: number) =>
   (rating === undefined ? '?' : rating.toFixed(0)) + '/5';
--- libs/review/ui-tile/src/lib/review-ui-tile.tsx
+++ libs/review/ui-tile/src/lib/review-ui-tile.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
-
 import styled from '@emotion/styled';
 
-/* eslint-disable-next-line */
-export interface ReviewUiTileProps {}
-
-const StyledReviewUiTile = styled.div`
-  color: pink;
+export const TileGrid = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  justify-content: space-evenly;
 `;
 
-export const ReviewUiTile = (props: ReviewUiTileProps) => {
-  return (
-    <StyledReviewUiTile>
-      <h1>Welcome to review-ui-tile component!</h1>
-    </StyledReviewUiTile>
-  );
-};
+export const Tile = styled.div`
+  border: solid 1px #ccc;
+  border-radius: 5px;
+  overflow: hidden;
+  display: block;
+  min-width: 200px;
+  color: inherit;
+  text-decoration: none;
+  margin: 0.5em;
+  &:hover {
+    border-color: #5159ea;
+  }
+`;
 
-export default ReviewUiTile;
+export const ImageWrapper = styled.div`
+  width: 200px;
+  height: 200px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: solid 1px #ccc;
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  min-height: 100%;
+`;
+
+export const TileTitle = styled.h2`
+  text-align: center;
+`;
+
+export const TileLeftCorner = styled.div`
+  display: block;
+  float: left;
+  margin: -0.5em 0 1em 0.5em;
+`;
+
+export const TileRightCorner = styled.div`
+  display: block;
+  float: right;
+  margin: -0.5em 0.5em 1em 0;
+`;
